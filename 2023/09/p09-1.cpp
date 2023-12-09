77b8cf9c34b2c6e511f73f3548bfc5b998b1731f
Leif Enblom <github@enblom.se>
2023-12-09 22:43:54 +0100

    AoC 2023 Day 9 Part 1

---
+++ 2023/09/p09-1.cpp
@@ -0,0 +1,62 @@
+// (c) Leif Enblom
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int64_t next_value(int64_t *in_seq, int in_values)
+{
+    int i;
+    int64_t new_seq[32];
+    int64_t value = 0;
+    bool all_zeros = true;
+
+    for (i = 0; i < in_values - 1; i++) {
+        new_seq[i] = in_seq[i + 1] - in_seq[i];
+        if (new_seq[i] != 0)
+            all_zeros = false;
+    }
+
+    if (!all_zeros)
+        value = in_seq[ in_values - 1] + next_value(new_seq, in_values - 1);
+    else
+        value = in_seq[0];
+
+    return value;
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *file;
+    char file_name[64];
+    char line[512];
+    char *cursor;
+    int n;
+    int64_t val;
+    int values;
+    int64_t seq[32];
+    int64_t total_value = 0;
+    int64_t value;
+
+    strcpy(file_name, "/home/dad/src/aoc/aoc-data/2023/p09-input.txt");
+
+    if ((file = fopen(file_name, "r")) != NULL) {
+        while (fgets(line, sizeof(line), file) != NULL) {
+            cursor = line;
+            values = 0;
+            while (sscanf(cursor, "%ld %n", &val, &n) == 1) {
+                seq[values] = val;
+                values++;
+                cursor += n;
+            }
+
+            value = next_value(seq, values);
+            total_value += value;
+        }
+
+        fclose(file);
+    }
+
+    printf("Total value: %ld\n", total_value);
+
+    return 0;
+}
