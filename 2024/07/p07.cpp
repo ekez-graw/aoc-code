defc0bead350d01e854358a88a46c0e5adfe34b3
Leif Enblom <github@enblom.se>
2024-12-08 12:19:51 +0100

    2024/07 P2

--- 2024/07/p07.cpp
+++ 2024/07/p07.cpp
@@ -17,6 +17,7 @@ int main(int argc, char** argv)
 
 	vector<long long> test_values(original_lines.size());
 	vector<vector<long long>> numbers(original_lines.size());
+	vector<vector<string>> numbers_as_strings(original_lines.size());
 
 	for (size_t i = 0; i < original_lines.size(); ++i) {
 		stringstream ss(original_lines[i]);
@@ -24,12 +25,16 @@ int main(int argc, char** argv)
 		if (getline(ss, number, ':'))
 			test_values[i] = stoll(number);
 		vector<long long> values;
+		vector<string> values_as_strings;
+		getline(ss, number, ' ');
 		while (getline(ss, number, ' ')) {
 			try {
 				values.push_back(stoll(number));
+				values_as_strings.push_back(number);
 			} catch (const invalid_argument& e) {}
 		}
 		numbers[i] = values;
+		numbers_as_strings[i] = values_as_strings;
 	}
 
 	for (size_t line = 0; line < original_lines.size(); ++line) {
@@ -51,6 +56,41 @@ int main(int argc, char** argv)
 	}
 
 	cout << "P07 Part 1: Grand total: " << grand_total << endl;
+
+	grand_total = 0;
+	for (size_t line = 0; line < original_lines.size(); ++line) {
+		unsigned long long number_of_permutations = 1 << (2 * (numbers[line].size() -1));
+
+		for (size_t i = 0; i < number_of_permutations; ++i) {
+			bool discard = false;
+			total = numbers[line][0];
+			for (size_t j = 1; j < numbers[line].size() && !discard; ++j) {
+				unsigned int oper = (i >> (2 * (j - 1))) & 0x3;
+				string s;
+				switch (oper) {
+				case 0:
+					total *= numbers[line][j];
+					break;
+				case 1:
+					total += numbers[line][j];
+					break;
+				case 2:
+					s = to_string(total) + numbers_as_strings[line][j];
+					total = stoll(s);
+					break;
+				case 3:
+					discard = true;
+					break;
+				};
+			}
+			if (!discard && (total == test_values[line])) {
+				grand_total += total;
+				break;
+			}
+		}
+	}
+
+	cout << "P07 Part 2: Grand total: " << grand_total << endl;
 
 	return 0;
 }
